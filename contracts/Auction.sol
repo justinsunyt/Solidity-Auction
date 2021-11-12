865d608d8cda4acf2aec597350bef4efc4a7b361
Justin Sun <justinsunyt@gmail.com>
2021-11-12 10:49:14 -0800

    Configure and emergencyEnd

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -5,19 +5,22 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract Auction {
-    IERC721 public immutable nft;
-    uint256 public immutable nftId;
+    IERC721 public nft;
+    uint256 public nftId;
     address payable public immutable owner;
     address public topBidder;
-    uint256 public immutable startPrice;
+    uint256 public startPrice;
     uint256 public currentPrice;
     uint256 public endTime;
     
-    constructor(IERC721 _nft, uint256 _nftId, uint256 _startPrice, uint256 daysFromNow) {
+    constructor() {
+        owner = payable(msg.sender);
+    }
+    
+    function configure(IERC721 _nft, uint256 _nftId, uint256 _startPrice, uint256 daysFromNow) public {
         require(_startPrice > 1, "Start price should be greater than 1");
         nft = IERC721(_nft);
         nftId = _nftId;
-        owner = payable(msg.sender);
         startPrice = _startPrice;
         currentPrice = _startPrice;
         endTime = block.timestamp + (daysFromNow * 1 days);
@@ -33,15 +36,19 @@ contract Auction {
         _;
     }
     
-    function bid(address bidder, uint256 price) public beforeEnd {
+    function bid(uint256 price) public beforeEnd {
         require(price > currentPrice, "The price is lower than the current price");
-        topBidder = bidder;
+        topBidder = msg.sender;
         currentPrice = price;
     }
     
     function end() public payable afterEnd {
-        nft.approve(topBidder, nftId);
-        nft.safeTransferFrom(owner, topBidder, nftId);
-        owner.transfer(msg.value);
+        nft.safeTransferFrom(address(this), topBidder, nftId);
+        owner.transfer(currentPrice);
+    }
+    
+    function emergencyEnd() public payable {
+        nft.safeTransferFrom(address(this), topBidder, nftId);
+        owner.transfer(currentPrice);
     }
 }
--- contracts/Database.sol
+++ contracts/Database.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.7.0 <0.9.0;
 import "/contracts/Auction.sol";
 
 contract Database {
-    Auction[] activeAuctions;
-    Auction[] pastAuctions;
-    address[] winners;
+    Auction[] public activeAuctions;
+    Auction[] public pastAuctions;
+    address[] public winners;
     
     function createAuction(Auction auction) public {
         require(activeAuctions.length < 30, "There are already 30 active auctions");
